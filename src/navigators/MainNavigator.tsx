6b09c7efa72be5f4c8476ba5268d9bfc9008141e
Den Lahodnyi <denlahodnyi.dev@gmail.com>
2024-01-20 16:49:50 +0200

    Install react-navigation

---
+++ src/navigators/MainNavigator.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export default function MainNavigator() {
+  return <Navigator>MainNavigator</Navigator>;
+}
