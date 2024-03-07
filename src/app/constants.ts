c4abcee3cc7ed01d5bae871dcde80da00c13a6c4
Den Lahodnyi <denlahodnyi.dev@gmail.com>
2024-03-07 12:59:34 +0200

    Add activities filtering
    Description:
    - add ability to filter activities by date and categories
    - fix useActivities hook, because it returned stale activities for different vehicles

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,3 +3,4 @@ export const COMMENT_LENGTH = 250;
 export const LOCATION_LENGTH = 100;
 export const CURRENCY_MINOR_UNITS = 100;
 export const CURRENCY_CODE = 'USD';
+export const APP_MIN_DATE_STR = '1950-01-01T00:01:00';
--- src/widgets/activities/feed/ui/Toolbar.tsx
+++ src/widgets/activities/feed/ui/Toolbar.tsx
@@ -1,31 +1,102 @@
+import { useMemo, useState } from 'react';
 import {
   type StyleProp,
   StyleSheet,
-  TouchableWithoutFeedback,
+  TouchableOpacity,
   View,
   type ViewStyle,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Icon } from '~/shared/components';
 import { toArray } from '~/shared/utils';
+import {
+  ActivitiesFiltersModal,
+  type FiltersState as ModalFiltersState,
+} from '~/features/activities/sort';
+
+type FiltersState = {
+  categoryIds: string[];
+  endDate: null | Date;
+  startDate: null | Date;
+};
 
 type ToolbarProps = {
+  canSort?: boolean;
   containerStyle?: StyleProp<ViewStyle>;
+  onFiltersApply?: (filters: FiltersState) => void;
+  selectedFilters?: FiltersState;
 };
 
 function Toolbar(props: ToolbarProps) {
-  const { containerStyle } = props;
+  const { canSort, containerStyle, onFiltersApply, selectedFilters } = props;
   const navigation = useNavigation();
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
+
+  const modalFiltersFromProp = useMemo<ModalFiltersState>(() => {
+    const { categoryIds, endDate, startDate } = selectedFilters || {
+      categoryIds: [],
+      endDate: null,
+      startDate: null,
+    };
+
+    return {
+      categoryIds,
+      dates: {
+        end: endDate || new Date(),
+        endModified: !!endDate,
+        start: startDate || new Date(),
+        startModified: !!startDate,
+      },
+    };
+  }, [selectedFilters]);
 
   const handleAddActivity = () => {
     navigation.navigate('Categories', { mode: 'create' });
   };
 
+  const handleApplyFilters = (filters: ModalFiltersState) => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { end, endModified, start, startModified } = filters.dates;
+    const { categoryIds } = filters;
+
+    setIsFilterModalOpen(false);
+    if (onFiltersApply) {
+      onFiltersApply({
+        categoryIds,
+        endDate: endModified ? end : null,
+        startDate: startModified ? start : null,
+      });
+    }
+  };
+
+  const handleToggleSortSettings = () =>
+    setIsFilterModalOpen(!isFilterModalOpen);
+
   return (
     <View style={[styles.container, ...toArray(containerStyle)]}>
-      <TouchableWithoutFeedback onPress={handleAddActivity}>
+      <TouchableOpacity
+        accessibilityLabel="Add new activity"
+        onPress={handleAddActivity}
+      >
         <Icon name="add" size={25} />
-      </TouchableWithoutFeedback>
+      </TouchableOpacity>
+      {canSort && (
+        <>
+          <View style={styles.divider} />
+          <TouchableOpacity
+            accessibilityLabel="Show sorting settings"
+            onPress={handleToggleSortSettings}
+          >
+            <Icon name="options" size={25} />
+          </TouchableOpacity>
+          <ActivitiesFiltersModal
+            selectedFilters={modalFiltersFromProp}
+            visible={isFilterModalOpen}
+            onApplyFilters={handleApplyFilters}
+            onClose={() => setIsFilterModalOpen(false)}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -34,9 +105,17 @@ export default Toolbar;
 
 const styles = StyleSheet.create({
   container: {
+    alignSelf: 'stretch',
     backgroundColor: '#FFF',
     borderRadius: 10,
+    flexDirection: 'row',
     paddingHorizontal: 12,
     paddingVertical: 6,
+  },
+  divider: {
+    borderColor: '#CECECE',
+    borderLeftWidth: 1,
+    height: '100%',
+    marginHorizontal: 6,
   },
 });
